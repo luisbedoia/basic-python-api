c83f35dc93845ffa9fcdb2f82095ac6536efd776
luisbedoia <ariasb.luis@gmail.com>
2021-10-26 10:26:52 -0500

    Unused stuff cleaned, sime fields switched to snake case.

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter, HTTPException, WebSocket
-from app.services.step import StepService, StepService2
-from app.spec.store import testStore
+from app.services.step import StepService
 import json
-import copy
 
 from app.models.user import user, user_in, users
 
-store = copy.deepcopy(testStore)
-service = StepService(store)
-service2 = StepService2(users)
+service = StepService(users)
 
 User = APIRouter()
 
-User.step = service2
+User.step = service
 
 @User.get("/users/{user}/steps")
 async def get_steps(user:str):
@@ -34,7 +30,7 @@ async def add_steps(websocket: WebSocket):
             data = await websocket.receive_text()
             data = json.loads(data)
             print(data)
-            await User.step.add(data['username'],data['ts'],data['newSteps'])
+            await User.step.add(data['username'],data['ts'],data['new_steps'])
         except Exception as e:
             print('error:', e)
             break
--- app/services/step.py
+++ app/services/step.py
@@ -1,32 +1,5 @@
-from app.models.user import user, user_in, users 
+from app.models.user import user, user_in, users
 class StepService:
-    def __init__(self, store: dict):
-        self.store = store
-
-    def get(self, username: str):
-        '''If user exist, returns the user info object, else returns None'''
-        if username in self.store:
-            return self.store[username]
-        else:
-            return None
-
-    def add(self, username: str, ts: float, newSteps: int):
-        '''If user exists, replace its timestamp ts and add newSteps to cumulativeSteps, else inserts a new user with incoming data'''
-        if username and ts and newSteps:
-            if username in self.store:
-                self.store[username]['ts'] = ts
-                self.store[username]['cumulativeSteps'] = self.store[username]['cumulativeSteps'] + newSteps
-                return True
-            else:
-                self.store[username] = {
-                    'ts': ts,
-                    'cumulativeSteps': newSteps
-                }
-                return True
-        else:
-            return False
-
-class StepService2:
     def __init__(self, User:users):
         self.User = users
 
@@ -38,15 +11,15 @@ class StepService2:
         else:
             return None
 
-    async def add(self, username: str, ts: float, newSteps: int) -> bool:
-        '''If user exists, replace its timestamp ts and add newSteps to cumulativeSteps, else inserts a new user with incoming data'''
-        if username and ts and newSteps:
+    async def add(self, username: str, ts: float, new_steps: int) -> bool:
+        '''If user exists, replace its timestamp ts and add new_steps to cumulative_steps, else inserts a new user with incoming data'''
+        if username and ts and new_steps:
             user_username = await self.User.filter(username=username).first()
             if user_username:
                 update = {
                     'username': username,
                     'ts': ts,
-                    'cumulativeSteps': user_username.cumulativeSteps + newSteps
+                    'cumulative_steps': user_username.cumulative_steps + new_steps
                 }
                 await self.User.filter(username=username).update(**update)
                 return True
@@ -54,7 +27,7 @@ class StepService2:
                 user = {
                     'username': username,
                     'ts': ts,
-                    'cumulativeSteps': newSteps
+                    'cumulative_steps': new_steps
                 }
                 new_user = await self.User.create(**user)
                 return True
